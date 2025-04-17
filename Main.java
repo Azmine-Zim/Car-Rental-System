e5226c067479832e440a4e5a4670faebefde74d6
unknown <mdazmainzim@gmail.com>
2025-04-17 19:51:21 +0800

    car class completed

---
+++ Main.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class car{
+    private String carID;
+    private String brand;
+    private String model;
+    private double basePricePerDay;
+    private boolean isAvailable;
+
+    public car(String carID,String brand,String model,double basePricePerDay){
+        this.carID=carID;
+        this.brand=brand;
+        this.basePricePerDay=basePricePerDay;
+        this.isAvailable=true;
+    }
+
+    public String  getCarId(){
+        return carID;
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public double CalculatePrice(int rentalDays){
+        return basePricePerDay*rentalDays;
+    }
+
+    public boolean isAvailable(){
+        return isAvailable;
+    }
+
+    public void rent(){
+        isAvailable=false;
+    }
+
+    public void returnCar(){
+        isAvailable=true;
+    }
+
+
+}
